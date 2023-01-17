262999e3cfbf3dc70ca19df4818e78ff7d3f9e80
RichardPMachado <richardmachado2211@gmail.com>
2023-01-17 00:09:46 -0300

    feat[requisito 2]' product modal.test

---
+++ tests/unit/models/productsModels.test.js
@@ -0,0 +1,36 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const chaiHttp = require('chai-http');
+const sinonChai = require('sinon-chai');
+const { productsModel } = require('../../../src/models');
+const connection = require('../../../src/models/connection');
+const productsMock = require('../../../__tests__/_dataMock');
+const { expect } = chai;
+
+
+describe('Model de Products', function () {
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  describe(" Listar todos os produtos", function () {
+    it("Deve retornar todos os produtos", async function () {
+      sinon
+        .stub(connection, "execute")
+        .resolves([productsMock.allProductsResponse]);
+
+      const result = await productsModel.findAll();
+      expect(result).to.be.deep.equal(productsMock.allProductsResponse);
+    });
+  });
+  describe(" Listar produto pelo id", function () {
+    it("Deve retornar o produto requerido", async function () {
+      sinon
+        .stub(connection, "execute")
+        .resolves([[productsMock.allProductsResponse[1]]]);
+
+      const result = await productsModel.findById(2);
+      expect(result).to.be.deep.equal(productsMock.allProductsResponse[1]);
+    });
+  });
+});
---
+++ tests/unit/services/productsServices.test.js
@@ -0,0 +1,45 @@
+const chai = require("chai");
+const sinon = require("sinon");
+const chaiHttp = require("chai-http");
+const sinonChai = require("sinon-chai");
+const { productsService } = require("../../../src/services");
+const { productsModel } = require("../../../src/models");
+const productsMock = require("../../../__tests__/_dataMock");
+const { findById } = require("../../../src/models/products.model");
+const { expect } = chai;
+
+
+describe("Service de Products", function () {
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  describe(" Listar todos os produtos", function () {
+    it("Deve retornar todos os produtos", async function () {
+      sinon.stub(productsModel, "findAll")
+        .resolves(productsMock.allProductsResponse);
+
+      const result = await productsService.findAll();
+      expect(result).to.be.deep.equal(productsMock.allProductsResponse);
+    });
+  });
+  
+  describe(" Listar produto pelo id", function () {
+    // it('Deve rotornar false se produto não existe', async function () {
+    //   sinon.stub(productsModel, 'findById')
+    //     .resolves(undefined);
+      
+    //   const result = await productsModel.findById(999);
+    //   expect(result).to.be.false;
+    // });
+
+    it("Deve retornar o produto requerido", async function () {
+      sinon
+        .stub(productsModel, "findById")
+        .resolves(productsMock.allProductsResponse[1]);
+
+      const result = await productsService.findById(2);
+      expect(result).to.be.deep.equal(productsMock.allProductsResponse[1]);
+    });
+  });
+});
